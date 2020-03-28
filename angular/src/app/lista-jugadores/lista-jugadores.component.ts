8aefea34e879e2676f89e94ebfb2d2e5daf8dbc5
Christophe Courvalet <chris.courvalet@madrid.scalefast.com>
2020-03-28 23:45:26 +0100

    Foreach power preparation to link player voted and sum afterward

--- angular/src/app/lista-jugadores/lista-jugadores.component.ts
+++ angular/src/app/lista-jugadores/lista-jugadores.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { JugadoresService } from '../jugadores/jugadores.service';
 import { VotacionAldeanosService } from '../votacion/votacion.service';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 import { Jugador } from '../types';
 import { Voto } from '../types';
 
@@ -15,15 +15,24 @@ export class ListaJugadoresComponent implements OnInit {
 
   jugadores: Observable<Jugador[]>;
   votacion_aldeanos: Observable<Voto[]>;
+  jugadores_l: Jugador[];
+  votacion_aldeanos_l: Voto[];
 
   constructor(private jugadoresService: JugadoresService, private votacionAldeanosService: VotacionAldeanosService) { }
 
   ngOnInit() {
-    this.jugadores = this.jugadoresService.getJugadores().pipe(
-      tap((wat) => {console.log(wat)})
-    );
-    this.votacion_aldeanos = this.votacionAldeanosService.getVotacions().pipe(
-      tap((wat) => {console.log(wat)})
-    );
+    this.jugadores = this.jugadoresService.getJugadores();
+    this.votacion_aldeanos = this.votacionAldeanosService.getVotacions();
+
+    this.jugadores.forEach(jugadores => {
+      jugadores.forEach(jugador => {
+        this.votacion_aldeanos.forEach(votacion_aldeanos => {
+          votacion_aldeanos.forEach(votacion_aldeano => {
+            console.log(votacion_aldeano.nominado);
+          });
+        });
+        console.log(jugador);
+      });
+    });
   }
 }
