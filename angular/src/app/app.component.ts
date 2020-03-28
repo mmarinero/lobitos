c8ca42bc4d4c6126fd633e4c5bbe0cc7eaf8f248
davidcanonieto <david.cano.nieto@gmail.com>
2020-03-28 14:18:08 +0000

    Estructura App

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lobitos';
+
+  mobileQuery: MediaQueryList;
+  title = 'Lobitos Game';
 }
---
+++ angular/src/app/lista-jugadores/lista-jugadores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-jugadores',
+  templateUrl: './lista-jugadores.component.html',
+  styleUrls: ['./lista-jugadores.component.scss']
+})
+export class ListaJugadoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
