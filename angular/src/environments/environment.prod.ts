0a8ae0aaa20c5b6e4a2219aaa7960c38589e6718
Mario Marinero <mario.marinero@scalefast.com>
2020-03-29 00:38:36 +0100

    deployment

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXZ80ywrHJasoG6L-DYmyDpTG72Sq0y-8",
+    authDomain: "lobitos-b1917.firebaseapp.com",
+    databaseURL: "https://lobitos-b1917.firebaseio.com",
+    projectId: "lobitos-b1917",
+    storageBucket: "lobitos-b1917.appspot.com",
+    messagingSenderId: "476348013767",
+    appId: "1:476348013767:web:b7b201909976644ca372e2",
+    measurementId: "G-S3T7HKHPDS"
+  }
 };
