ca843b4d3e3e2e3d76701604a278e4a52734fa25
Mario Marinero <mario.marinero@scalefast.com>
2020-03-29 01:58:39 +0100

    Fusilar angular-firebase-starter

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuardService } from 'src/app/services/auth-guard.service';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { VerifyEmailComponent } from './pages/verify-email/verify-email.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full', canActivate: [AuthGuardService]},
+  { path: 'register', component: RegistrationComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,20 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   title = 'Lobitos Game';
+  user = this.authService.user;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) {
+      console.error(this.authService.user)
+    }
+
+  goToLogin() {
+    this.router.navigate(['login']);
+  }
+
+  logOut() {
+    this.authService.SignOut();
+  }
 }
